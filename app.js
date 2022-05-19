f684217cab313d861be3a9350e9db3e1aacc728b
RoniGraizman <ronigraiz@gmail.com>
2022-05-19 20:33:01 -0300

    proyecto

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname,'public')))
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log ('corriendo en el puerto ' + PORT)
+})
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname,'/views/home.html'))
+})
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname,'views/register.html'))
+})
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname,'views/login.html'))
+})
