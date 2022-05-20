803044601fb36ead9f923e3a07442dc47e9bcdc1
RoniGraizman <ronigraiz@gmail.com>
2022-05-19 21:16:47 -0300

    make it better

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express()
 
 app.use(express.static(path.join(__dirname,'public')))
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(PORT, () => {
     console.log ('corriendo en el puerto ' + PORT)
 })
 
